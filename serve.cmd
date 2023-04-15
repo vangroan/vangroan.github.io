e8006ac490077a2413b5ec8228c9b102d96e7d22
Willem Victor <wimpievictor@gmail.com>
2023-04-15 13:30:03 +0200

    Initial commit

---
+++ serve.cmd
@@ -0,0 +1,17 @@
+echo off
+rem Serve a development server
+
+set PY=
+set PELICAN=pelican
+set PELICANOPTS=
+
+set BASEDIR=%cd%
+set INPUTDIR=%BASEDIR%\content
+set OUTPUTDIR=%BASEDIR%\output
+set CONFFILE=%BASEDIR%\pelicanconf.py
+set PUBLISHCONF=%BASEDIR%\publishconf.py
+
+set GITHUB_PAGES_BRANCH=main
+
+
+%PELICAN% -l %INPUTDIR% -o %OUTPUTDIR% -s %CONFFILE% %PELICANOPTS%
